6d5eba7a6c6df3fb0031349886f70e00170f53f6
Silvia Teletor <teletorsilvia03@gmail.com>
2026-02-13 18:47:13 +0100

    I transliterate the code in js and edit css to center the text

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
-const listContainer = document.getElementById('listContainer')
-
-
+/*const container = document.getElementById("container")
 
 const getData = async () => {
-  try {
-    /* const response = await fetch('https://jsonplaceholder.typicode.com/posts'); */
-    const response = await fetch('https://rickandmortyapi.com/api/character');
-    const data = await response.json();
-    
-    
-    listContainer.innerHTML = data.results?.map((item) => {
-      return(
-        `<div class="characters">
-          <image src=${item.image} />
-          <p>${item.name}</p>
-        </div>`
+  try{
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const 
+    theDates = await response.json();
+    console.log(theDates)
+
+    container.innerHTML = theDates.map((item) => {
+      return (
+        `<div class="information">
+        <p>${item.title}</p>
+        <p>${item.body}</p>
+        </div>
+        `
       )
     })
-
-
   } catch (error) {
     console.error(error)
-}
-
+  }
 };
+getData()*/
 
-
+const listContainer = document.getElementById("listContainer")
+const getData = async () => {
+  try{
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    console.log(data)
+    listContainer.innerHTML = data.results.map((item) => {
+      return (
+        `<div class="characters>
+      <img src=${item.image} />
+      <p>${item.name}</p>
+      <p>${item.status}</p>
+      <p>${item.species}</p>
+      <p>${item.gender}</p>
+      </div>`
+      )
+      
+    })
+  } catch (error) {
+    console.error(error)
+  }
+};
 getData()
